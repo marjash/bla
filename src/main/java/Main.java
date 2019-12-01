362261d982d609de6881b17c0a11ba58459b89ff
marjash <marjash@i.ua>
2019-12-01 14:58:57 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
+//         hello from git
         Class.forName("com.mysql.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/users";
         String user = "root";
