52550463d51666c2bca49d609d9b024d70b62697
marjash <marjash@i.ua>
2019-12-01 14:55:34 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/users";
+        String user = "root";
+        String password = "root";
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from person");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.print(resultSet.getString(1) + " ");
+            System.out.print(resultSet.getString(2) + " ");
+            System.out.println(resultSet.getString(3));
+        }
+        connection.close();
+
+    }
+}
